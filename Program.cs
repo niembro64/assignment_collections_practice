09c694021e44547e946fab31bc7869b8911ddc5a
=Eric Niemeyer <=niemeyer.eric@gmail.com>
2022-04-04 14:56:20 -0400

    init

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace assignment_collections_practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] nums;
+            nums = new int[10];
+            for (var i = 0; i < 10; i++)
+            {
+                nums[i] = i + 1;
+                Console.Write($"{i} ");
+            }
+            
+
+        }
+    }
+}
