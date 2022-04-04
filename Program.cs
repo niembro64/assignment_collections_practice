ccc410888a77b0f71a0f5f5ba6512cd12f655b11
=Eric Niemeyer <=niemeyer.eric@gmail.com>
2022-04-04 15:02:57 -0400

    tf

--- Program.cs
+++ Program.cs
@@ -9,12 +9,39 @@ namespace assignment_collections_practice
 
             int[] nums;
             nums = new int[10];
-            for (var i = 0; i < 10; i++)
+            for (int i = 0; i < 10; i++)
             {
                 nums[i] = i + 1;
                 Console.Write($"{i} ");
             }
-            
+
+            /////////////////////////////////////////////
+            Console.WriteLine();
+
+            string[] names;
+            names = new string[4];
+            names[0] = "Tim";
+            names[1] = "Martin";
+            names[2] = "Nikki";
+            names[3] = "Sara";
+
+            for (int i = 0; i < 4; i++){
+                Console.Write($"{i} {names[i]} \n");
+            }
+
+            /////////////////////////////////////////////
+            Console.WriteLine();
+
+            bool[] alt;
+            alt = new bool[10];
+            for (int i = 0; i < 10; i++){
+                alt[i] = (i % 2 == 0 ? true : false);
+                Console.Write($"{i} {alt[i]} \n");
+            }
+
+
+
+
 
         }
     }
